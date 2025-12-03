7e5271fedd6ebb17b55ac1b4ed68c2caf5319747
kastes <kastes68@mail.ru>
2025-12-03 22:57:27 +0300

    5.5 Знакомство с gRPC протоколом. Упражнения

---
+++ examples/grpc/grpc_course_client.py
@@ -0,0 +1,11 @@
+import course_service_pb2
+import course_service_pb2_grpc
+import grpc
+
+# Устанавливаем соединение с сервером
+channel = grpc.insecure_channel("localhost:50051")
+stub = course_service_pb2_grpc.CourseServiceStub(channel)
+
+# Отправляем запрос
+response = stub.GetCourse(course_service_pb2.GetCourseRequest(course_id="api-course"))
+print(response)
