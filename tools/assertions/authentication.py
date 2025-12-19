ab220b50b37de5aa9e9d4e2ac3d50cec76a3a7ef
kastes <kastes68@mail.ru>
2025-12-19 15:45:33 +0300

    8.2 Написание API автотестов используя Pytest. Практика

---
+++ tools/assertions/authentication.py
@@ -0,0 +1,19 @@
+from clients.authentication.authentication_schema import (
+    LoginResponseSchema,
+)
+from tools.assertions.base import assert_equal, assert_is_true
+
+
+def assert_login_response(response: LoginResponseSchema) -> None:
+    """
+    Проверить данные ответа сценария 'успешная аутентификация пользователя'
+
+    Args:
+        response (LoginResponseSchema): данные ответа
+
+    Raises:
+        AssertionError: если данные ответа не соответствуют ожидаемым
+    """
+    assert_equal(response.token.token_type, "bearer", "token_type")
+    assert_is_true(response.token.access_token, "access_token")
+    assert_is_true(response.token.refresh_token, "refresh_token")
--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -29,3 +29,15 @@ def assert_equal(actual: Any, expected: Any, name: str) -> None:
     :raises AssertionError: если значения не равны
     """
     assert actual == expected, f"Имя: {name}. Ожидался: {expected}. Получен {actual}"
+
+
+def assert_is_true(actual: Any, name: str) -> None:
+    """
+    Проверить значение на истинность
+
+    Args:
+        actual (Any): Значение для проверки
+        name (str): Имя
+    Raises AssertionError: если значение не приводится к True
+    """
+    assert actual, f"Имя: {name}. Ожидался: {True}. Получен {actual}"
