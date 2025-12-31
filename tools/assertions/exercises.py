ed379e8e431595d594871b3ae98169e3186121fb
kastes <kastes68@mail.ru>
2025-12-31 16:11:28 +0300

    9.4 Практикуемся в написании API автотестов. Часть 3. Создать упражнение

---
+++ tools/assertions/exercises.py
@@ -0,0 +1,48 @@
+from clients.exercises.exercises_schema import (
+    CreateExerciseRequestSchema,
+    CreateExerciseResponseSchema,
+    ExerciseSchema,
+)
+from tools.assertions.base import assert_equal, assert_is_true
+
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema) -> None:
+    """
+    Проверить данные 'упражнение' на равенство
+
+    Args:
+        actual (ExerciseSchema): фактические данные 'упражнение'
+        expected (ExerciseSchema): ожидаемые данные 'упражнение'
+    Raises:
+        AssertionError: если фактические и ожидаемые данные не равны
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.order_index, expected.order_index, "order_index")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+
+
+def assert_create_exercise_response(
+    actual: CreateExerciseResponseSchema, expected: CreateExerciseRequestSchema
+) -> None:
+    """
+    Проверить что данные ответа 'создать упражнение' соответствуют ожидаемым
+
+    Args:
+        actual (CreateExerciseResponseSchema): данные ответа 'создать упражнение'
+        expected (CreateExerciseRequestSchema): данные запроса 'создать упражнение'
+    Raises:
+        AssertionError: если данные не ответа не соответствуют ожидаемым
+    """
+    assert_is_true(actual.exercise.id, "id")
+    assert_equal(actual.exercise.title, expected.title, "title")
+    assert_equal(actual.exercise.course_id, expected.course_id, "course_id")
+    assert_equal(actual.exercise.max_score, expected.max_score, "max_score")
+    assert_equal(actual.exercise.min_score, expected.min_score, "min_score")
+    assert_equal(actual.exercise.order_index, expected.order_index, "order_index")
+    assert_equal(actual.exercise.description, expected.description, "description")
+    assert_equal(actual.exercise.estimated_time, expected.estimated_time, "estimated_time")
